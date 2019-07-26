f964b9b59eb90695f3342035005f10070011e752
davidshin <davidshin@192.168.99.1>
2019-07-26 17:28:54 +0900

    test

--- DavidAppServer2/src/main/java/DavidApp/DavidAppServer/App.java
+++ DavidAppServer2/src/main/java/DavidApp/DavidAppServer/App.java
@@ -92,6 +92,7 @@ public class App {
 	    public void run(String... strings)throws Exception{
 	    	
 	    	System.out.println("test");
+	    	System.out.println("test");
 
 	        Stream.of("Conrad","Hilton","Shilla","Hayatt","Westin","Sangrila").forEach(name-> repository.save(new Hotel(name)));
 
